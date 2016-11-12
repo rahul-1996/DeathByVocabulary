48f7d41a4f282d658ff1583aeb59c58b1dbb6645
Shreyas Anupindi <Shreyasanupindi356@gmail.com>
2016-11-12 21:56:45 +0530

    Fifth commit-generate function

--- app/views.py
+++ app/views.py
@@ -28,23 +28,24 @@ G=nx.read_gpickle('test2.gpickle')
 
 @app.route('/')
 def gg():
-    node1=random.choice(G.nodes())
-    words=G.neighbors(node1)
-    while(not G.neighbors(node1) or len(G.neighbors(node1)) < 15):
-        node1 = random.choice(G.nodes())
-    print(node1)
-    check=random.randint(4,8)
-    print(check)
-    visited=[node1]
-    for i in range(check):
-        randChoice=random.choice(G.neighbors(node1))
-        while((randChoice in visited) or (not G.neighbors(randChoice)) or (len(G.neighbors(randChoice)) < 15)):
-            if(len(G.neighbors(randChoice))==1):
-                break
+    flag=True
+    while flag:
+        node1=random.choice(G.nodes())
+        while(not G.neighbors(node1) or len(G.neighbors(node1)) < 20):
+            node1 = random.choice(G.nodes())
+        print(node1)
+        check=random.randint(4,8)
+        print(check)
+        visited=[node1]
+        for i in range(check):
             randChoice=random.choice(G.neighbors(node1))
-        print(randChoice)
-        visited.append(randChoice)
-        node1 = randChoice
-    print(visited[check])
-    words=transformWord(G,visited[0],visited[check])
+            while((randChoice in visited) or (not G.neighbors(randChoice)) or (len(G.neighbors(randChoice)) < 20)):
+                randChoice=random.choice(G.neighbors(node1))
+            print(randChoice)
+            visited.append(randChoice)
+            node1 = randChoice
+        print(visited[check])
+        words=transformWord(G,visited[0],visited[check])
+        if(len(words) > 4):
+            flag = False
     return render_template('index.html',words = map(json.dumps,words))
